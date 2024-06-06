f5952bb1cf710dbd06b1af5428b3226338746dfb
Mihail-21 <139543682+Mihail-21@users.noreply.github.com>
2024-06-06 13:36:53 +0300

    hmwrk

---
+++ src/components/App/App.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+
+const dataContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
+
+const getSavedContacts = () => {
+  const savedContacts = localStorage.getItem("savedContacts");
+  return savedContacts ? JSON.parse(savedContacts) : dataContacts;
+};
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [contacts, setContacts] = useState(getSavedContacts);
+
+  const handleDelete = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("savedContacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const searchByName = contacts.filter(({ name }) => {
+    return name.toLowerCase().includes(inputValue.toLowerCase());
+  });
+  console.log(contacts);
+
+  const addContact = (contact) => {
+    setContacts((prev) => [...prev, contact]);
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm
+          contacts={contacts}
+          setContacts={setContacts}
+          addContact={addContact}
+        />
+        <SearchBox value={inputValue} onInput={setInputValue} />
+        <ContactList contacts={searchByName} onDelete={handleDelete} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import css from "./Contact.module.css";
+
+export default function Contact({ name, number, id, onDelete }) {
+  return (
+    <>
+      <div className={css.contactBox}>
+        <p>{name}</p>
+        <p>{number}</p>
+      </div>
+      <button onClick={() => onDelete(id)}>Delete</button>
+    </>
+  );
+}
